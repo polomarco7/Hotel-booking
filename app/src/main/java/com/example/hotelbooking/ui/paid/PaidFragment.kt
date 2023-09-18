19e7d459edefc17b1b62f86ca45b6687ca5b516e
Nikolay <Nikolay@VESTA>
2023-09-18 05:21:15 +0300

    fix paid

--- app/src/main/java/com/example/hotelbooking/ui/paid/PaidFragment.kt
+++ app/src/main/java/com/example/hotelbooking/ui/paid/PaidFragment.kt
@@ -5,15 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.hotelbooking.R
+import com.example.hotelbooking.databinding.FragmentHotelBinding
+import com.example.hotelbooking.databinding.FragmentPaidBinding
+import com.example.hotelbooking.ui.hotel.HotelViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PaidFragment : Fragment() {
+    private var _binding: FragmentPaidBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: PaidViewModel by viewModels()
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_paid, container, false)
+        _binding = FragmentPaidBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+
+        binding.payBtn.setOnClickListener {
+            findNavController().navigate(R.id.hotelFragment)
+        }
     }
 }
